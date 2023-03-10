64b02b87e942a19c8e8c4fcbefa8f22a9a1b16db
xitarps <xitarps@gmail.com>
2023-03-09 23:32:16 -0300

    [Fix]: Adjust simple insterest exmaple '*'

--- examples/simple_interest.rb
+++ examples/simple_interest.rb
@@ -2,7 +2,7 @@ def simple_interest
   puts <<-EOF
     Faça um algoritmo que calcule juros simples:
 
-    montante = CapitalInicial.(1 + jurosPorcento*periodo)
+    montante = CapitalInicial * (1 + jurosPorcento*periodo)
 
     ex: Tendo capital inicial 1000R$ com 5% de juros ao mês por 3 meses;
 
