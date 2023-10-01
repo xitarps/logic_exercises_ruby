11eed69623b7e1ebf92cf0e074dc77f82a5a6584
XitaRps <xitarps@gmail.com>
2023-10-01 20:17:21 -0300

    Update even_or_odd exmaple

--- examples/even_or_odd.rb
+++ examples/even_or_odd.rb
@@ -3,5 +3,10 @@ def even_or_odd
     Faça um algoritmo que diga se um numero é par ou impar:
 
     ex: EvenOrOdd.calculate(3) = '3 is odd'
+
+    * Considere como regra de negocio:
+    Caso seja um decimal, o número após a virgula será usado para
+    definir se é par ou impar
+    ex: X,22 => 22 é par, logo X,22 será considerado par
   EOF
 end
