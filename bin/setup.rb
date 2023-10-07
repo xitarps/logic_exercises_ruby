a65ae876ab568feecd2d074d5cb291fc6687991e
XitaRps <xitarps@gmail.com>
2023-10-07 10:20:38 -0300

    Update setup.rb to describe to user whats happening on install process

--- bin/setup.rb
+++ bin/setup.rb
@@ -1,15 +1,28 @@
 # lógica de instalação
+puts('Durante essa instalação será pedido pedido senha para dar permissão de execução do arquivo teste.sh :)')
+sleep(2)
+
+puts("\n")
+puts('Clonado repositório...')
 system('git clone https://github.com/xitarps/logic_exercises_ruby')
+
+puts("\n")
+puts('Adicionando permissão de execução no arquivo teste.sh, entre com sua senha de usuário')
 system("sudo chmod +x logic_exercises_ruby/test.sh")
 
+puts("\n")
 puts('Baixando libs para testes')
 system('gem install rspec')
+sleep(2)
 
-sleep(2)
 system('clear')
-system('cd logic_exercises_ruby')
 puts('Tudo pronto :)')
-puts('')
 sleep(2)
-puts('para começar basta digitar no terminal: ')
+
+puts("\n")
+puts('Para começar entre na pasta logic_exercises_ruby ')
+puts('cd ./logic_exercises_ruby ')
+puts("\n")
+
+puts('e execute o comando: ')
 puts('./test.sh')
